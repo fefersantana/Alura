ce1c3eac99896f69c91ca0d2395b37aba11640bb
Fefe <fernandarolimsantana@gmail.com>
2025-08-28 17:25:25 -0700

    finalizar funcionalidades basicas

---
+++ ingresso/js/app.js
@@ -0,0 +1,46 @@
+let maxPista = document.getElementById('qtd-pista').textContent = 100;
+let maxSuperior = document.getElementById('qtd-superior').textContent = 200;
+let maxInferior = document.getElementById('qtd-inferior').textContent = 400;
+
+function comprar(){
+    //pegar parametros
+    let ingresso = document.getElementById('tipo-ingresso').value;
+    let quantidade = document.getElementById('qtd').value;
+
+    //diminuir do total
+    switch (ingresso){
+        case "pista":{
+            maxPista = checarAssentos(quantidade,maxPista);
+            break;
+        }
+        case "superior": {
+            maxSuperior = checarAssentos(quantidade,maxSuperior);
+            break;
+        }
+        case "inferior": {
+            maxInferior = checarAssentos(quantidade,maxInferior);
+            break;
+        }
+    }
+
+    //display diminuido do total
+    let campoPista = document.getElementById('qtd-pista');
+    let campoSuperior = document.getElementById('qtd-superior');
+    let campoInferior = document.getElementById('qtd-inferior');
+
+    campoPista.textContent = maxPista;
+    campoSuperior.textContent = maxSuperior;
+    campoInferior.textContent = maxInferior;
+
+    //resetar qtd cadeiras
+    document.getElementById('qtd').value = '';
+}
+
+function checarAssentos (quantidade, max){
+    if (quantidade > max){
+        alert('Nao e possivel comprar mais assentos do que os disponiveis.'); 
+    } else {
+        max = max - quantidade;
+    }
+    return max;
+}
